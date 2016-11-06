08f6ae310b8044ee01c1d6a43b92b689b6cfd591
Erwin Rosales <look4erwin@gmail.com>
2016-11-06 01:51:43 -0800

    Implemented handleInput movement UP, DOWN, LEFT, RIGHT controls for player. Successfully bound player to canvas.

--- js/app.js
+++ js/app.js
@@ -3,12 +3,13 @@ var Enemy = function(loc) {
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
   this.x = 0;
-  this.y = loc * 100;
+  this.y = ((loc * 85) + 60);
+  this.speed = Math.random();
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
   this.sprite = 'images/enemy-bug.png';
-  this.width = this.sprite.width;
-  this.height = this.sprite.height;
+  this.enemyWidth = this.sprite.width;
+  this.enemyHeight = this.sprite.height;
 };
 
 // Update the enemy's position, required method for game
@@ -18,6 +19,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed + (dt * 200);
+    if(this.x > 500) {
+      this.x = this.x - 671;
+      this.x += Math.random() + (dt * 200);
+    }
 };
 // Draw the enemy on the screen, required method for game
 
@@ -25,16 +31,63 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class
 
 var Player = function() {
   this.x = 200;
   this.y = 400;
   this.sprite = 'images/char-boy.png';
-  this.width = this.sprite.width;
-  this.height = this.sprite.height;
+}
+
+Player.prototype.moveLEFT = function() {
+  this.x -= 100;
+  if(this.x < 0) {
+    this.x = 0;
+  }
+}
+
+Player.prototype.moveUP = function() {
+  this.y -= 85;
+  if(this.y < 0) {
+    this.y = 400;
+  }
+}
+
+Player.prototype.moveDOWN = function() {
+  this.y += 85;
+  if(this.y > 400) {
+    this.y = 400;
+  }
+}
+
+Player.prototype.moveRIGHT = function() {
+  this.x += 100;
+  if(this.x > 400) {
+    this.x = 400;
+  }
+}
+
+Player.prototype.handleInput = function(allowedKeys) {
+
+  switch(allowedKeys) {
+    case 'up':
+      this.moveUP();
+      console.log(this.pos);
+      break;
+    case 'down':
+      this.moveDOWN();
+      break;
+    case 'left':;
+      this.moveLEFT();
+      break;
+    case 'right':
+      this.moveRIGHT();
+      break;
+    default:
+      console.log('not an allowed key');
+  }
+  // Testing purposes DELETE
+  console.log(this.x + " " + this.y);
 }
 
 Player.prototype.update = function(dt) {
@@ -45,15 +98,16 @@ Player.prototype.render = function(dt) {
 };
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// An array called allEnemies
+// A player object in a variable called player
 
-var allEnemies = [new Enemy(0)];
+var allEnemies = [new Enemy(0), new Enemy(1), new Enemy(2)];
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
+  e.preventDefault();
     var allowedKeys = {
         37: 'left',
         38: 'up',
